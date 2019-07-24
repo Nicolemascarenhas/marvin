6eb678423d6b61ed5a43f09411ef521f5d337c85
Nicolemascarenhas <52723088+Nicolemascarenhas@users.noreply.github.com>
2019-07-24 19:57:41 -0300

    vogal ou consoante

--- fase04/ex03/vogalOuConsoante.js
+++ fase04/ex03/vogalOuConsoante.js
@@ -1,15 +1,25 @@
 function vogalOuConsoante(letra){
     if (letra == "a")
-    return ("Vogal");
+    return (Vogal);
     if (letra == "e")
-    return ("vogal");
+    return (vogal);
     if (letra == "i")
-    return ("vogal");
+    return (vogal);
      if (letra == "o")
-    return ("vogal");
+    return (vogal);
      if (letra == "u")
-    return ("vogal");
+    return (vogal);
+     if (letra == "A")
+    return (Vogal);
+    if (letra == "E")
+    return (vogal);
+    if (letra == "I")
+    return (vogal);
+     if (letra == "O")
+    return (vogal);
+     if (letra == "U")
+    return (vogal);
  else {
-    return ("Consoante");
+    return (Consoante);
     }
 }
