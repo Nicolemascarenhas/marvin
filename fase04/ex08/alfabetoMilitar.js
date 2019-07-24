2aac6f230541e01da7aea82e6ed51be896a3c654
Nicolemascarenhas <52723088+Nicolemascarenhas@users.noreply.github.com>
2019-07-24 19:26:36 -0300

    alpha

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,25 @@
+function alfabetoMilitar(vogal){
+    if (vogal=="a")
+    return "alpha";
+    if (vogal=="e")
+    return "echo";
+    if (vogal=="i")
+    return "india";
+    if (vogal=="o")
+    return "oscar";
+    if (vogal=="u")
+    return "uniform";
+    if (vogal=="A")
+    return "alpha";
+    if (vogal=="E")
+    return "echo";
+    if (vogal=="I")
+    return "india";
+    if (vogal=="O")
+    return "oscar";
+    if (vogal=="U")
+    return "uniform";
+    else{
+        return "false";
+    }
+}
