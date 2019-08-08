4b270ac9255d962d68d4c52f88915074fc1ef6ce
Nicolemascarenhas <52723088+Nicolemascarenhas@users.noreply.github.com>
2019-08-07 21:29:00 -0300

    numero primo

---
+++ fase06/ex03/primo.js
@@ -0,0 +1,23 @@
+function primo (num1){
+    var i = 2
+    var primo = false
+    if (num1==2) {
+        primo = true;
+        return "Sim"
+    }
+   
+    while (i < num1){    
+    if (num1 % i === 0){
+        primo = false;
+        break;
+        }else{
+        primo = true;
+       i++
+    }
+    }
+    if (primo){
+        return "Sim";
+    }else{
+        return "Não";
+    }
+}
